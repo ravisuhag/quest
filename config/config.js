dfc8bf9f035d5290ecec4008887a067b0e2b9389
Ravi Suhag <suhag.ravi@gmail.com>
2014-04-19 14:37:00 +0530

    Finishing Node app Setup

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
 var path = require('path'),
-  rootPath = path.normalize(__dirname + '/../..');
+  rootPath = path.normalize(__dirname + '/..');
 
 module.exports = {
   root: rootPath,
